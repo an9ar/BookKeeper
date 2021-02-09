44c48b2462dc2271382420708d9f1444da860052
Dmitriy <63235125+an9ar@users.noreply.github.com>
2021-02-09 10:28:19 +0300

    Added custom theme files

--- app/src/main/java/com/an9ar/bookkeeper/MainActivity.kt
+++ app/src/main/java/com/an9ar/bookkeeper/MainActivity.kt
@@ -4,35 +4,18 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.setContent
-import androidx.compose.ui.tooling.preview.Preview
-import com.an9ar.bookkeeper.ui.theme.BookKeeperTheme
+import com.an9ar.bookkeeper.theme.BookKeeperTheme
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             BookKeeperTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    BookKeeperTheme {
-        Greeting("Android")
     }
 }
