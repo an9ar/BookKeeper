4deada6f71da09bdf7b07db104a1f1c0587d4536
Dmitriy <63235125+an9ar@users.noreply.github.com>
2021-02-11 21:37:07 +0300

    Added BookModel

---
+++ app/src/main/java/com/an9ar/bookkeeper/data/BookModel.kt
@@ -0,0 +1,11 @@
+package com.an9ar.bookkeeper.data
+
+import io.realm.RealmObject
+import io.realm.annotations.PrimaryKey
+
+open class BookModel(
+    @PrimaryKey var id: Long = 0L,
+    var title: String = "",
+    var previewUrl: String = "",
+    var comment: String = ""
+): RealmObject()
