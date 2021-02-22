91508737c837b4ccd577113ac81afdba5846bc66
Dmitriy <63235125+an9ar@users.noreply.github.com>
2021-02-22 23:20:01 +0300

    Added Realm configuration

--- app/src/main/java/com/an9ar/bookkeeper/App.kt
+++ app/src/main/java/com/an9ar/bookkeeper/App.kt
@@ -2,6 +2,15 @@ package com.an9ar.bookkeeper
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
+import io.realm.Realm
+import io.realm.RealmConfiguration
 
 @HiltAndroidApp
-class App : Application()
+class App : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        Realm.init(this)
+        val config = RealmConfiguration.Builder().build()
+        Realm.setDefaultConfiguration(config)
+    }
+}
