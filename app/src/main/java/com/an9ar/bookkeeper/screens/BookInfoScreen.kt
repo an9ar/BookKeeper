cd24f295a0bb18ab9765271e8fddce1d5eac5b26
Dmitriy <63235125+an9ar@users.noreply.github.com>
2021-03-09 01:13:26 +0300

    Added BookInfoScreen content

--- app/src/main/java/com/an9ar/bookkeeper/screens/BookInfoScreen.kt
+++ app/src/main/java/com/an9ar/bookkeeper/screens/BookInfoScreen.kt
@@ -1,16 +1,29 @@
 package com.an9ar.bookkeeper.screens
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Scaffold
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
+import com.an9ar.bookkeeper.data.models.BookModel
+import com.an9ar.bookkeeper.theme.AppTheme
+import com.an9ar.bookkeeper.ui.BookScreenImage
+import com.an9ar.bookkeeper.ui.BookScreenInputField
+import com.an9ar.bookkeeper.ui.BookScreenSubmitButton
+import com.an9ar.bookkeeper.ui.BookScreenToolbar
 import com.an9ar.bookkeeper.viewmodels.MainViewModel
+import io.realm.Realm
 
 @Composable
 fun BookInfoScreen(
     bookId: String,
+    realmObject: Realm,
     navHostController: NavHostController,
     mainViewModel: MainViewModel
 ) {
+    val targetBook = mainViewModel.getBookById(id = bookId)
     Scaffold(
         topBar = {
             BookScreenToolbar(
@@ -19,17 +32,80 @@ fun BookInfoScreen(
             )
         }
     ) {
-        BookInfoScreenContent(
-            navHostController = navHostController,
-            mainViewModel = mainViewModel
-        )
+        targetBook?.let {
+            BookInfoScreenContent(
+                realmObject = realmObject,
+                navHostController = navHostController,
+                mainViewModel = mainViewModel,
+                bookModel = it
+            )
+        }
     }
 }
 
 @Composable
 fun BookInfoScreenContent(
+    realmObject: Realm,
     navHostController: NavHostController,
-    mainViewModel: MainViewModel
+    mainViewModel: MainViewModel,
+    bookModel: BookModel
 ) {
-
+    val bookData by remember { mutableStateOf(bookModel) }
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(AppTheme.colors.background)
+    ) {
+        var isValidated by remember { mutableStateOf(false) }
+        BookScreenImage(
+            scope = this,
+            initImage = bookData.previewUrl,
+            onImagePreviewChanged = { changedImage ->
+                realmObject.executeTransaction {
+                    bookData.previewUrl = changedImage
+                }
+            }
+        )
+        BookScreenInputField(
+            scope = this,
+            initValue = bookData.title,
+            label = "Book title",
+            isValidated = isValidated,
+            onInputValueChanged = { changedString ->
+                realmObject.executeTransaction {
+                    bookData.title = changedString
+                }
+            }
+        )
+        BookScreenInputField(
+            scope = this,
+            initValue = bookData.author,
+            label = "Book author",
+            isValidated = isValidated,
+            onInputValueChanged = { changedString ->
+                realmObject.executeTransaction {
+                    bookData.author = changedString
+                }
+            }
+        )
+        BookScreenInputField(
+            scope = this,
+            initValue = bookData.comment,
+            label = "Your short description (optional)",
+            onInputValueChanged = { changedString ->
+                realmObject.executeTransaction {
+                    bookData.comment = changedString
+                }
+            }
+        )
+        BookScreenSubmitButton(
+            scope = this,
+            onSubmitClick = {
+                isValidated = true
+                if (bookData.title.isNotEmpty() && bookData.author.isNotEmpty()) {
+                    navHostController.navigateUp()
+                }
+            }
+        )
+    }
 }
