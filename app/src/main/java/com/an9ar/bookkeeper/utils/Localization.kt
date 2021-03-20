322d46c60f7662a2482aea5b9c32b86254b166b5
Dmitriy <63235125+an9ar@users.noreply.github.com>
2021-03-20 19:17:18 +0300

    Added localization file

---
+++ app/src/main/java/com/an9ar/bookkeeper/utils/Localization.kt
@@ -0,0 +1,76 @@
+package com.an9ar.bookkeeper.utils
+
+// Temporary solution from https://github.com/TechnokratosDev/jetpack-compose-localization
+
+import java.util.Locale
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.ReadOnlyComposable
+import androidx.compose.runtime.compositionLocalOf
+
+private val defaultLocalization: Localization = Localization(Locale.ENGLISH)
+
+private val supportedLocales: MutableSet<Locale> = mutableSetOf()
+
+private val localizationMap = hashMapOf<Locale, Localization>()
+
+data class Localization(
+    val locale: Locale,
+    internal val strings: MutableMap<String, String> = mutableMapOf()
+)
+
+fun registerSupportedLocales(vararg locales: Locale): Set<Locale> {
+    locales.filter { it != Locale.ENGLISH }
+        .forEach {
+            if (supportedLocales.add(it)) {
+                registerLocalizationForLocale(it)
+            }
+        }
+    return supportedLocales + Locale.ENGLISH
+}
+
+private fun registerLocalizationForLocale(locale: Locale) {
+    localizationMap[locale] = Localization(locale)
+}
+
+fun Translatable(
+    name: String,
+    defaultValue: String,
+    localeToValue: () -> Map<Locale, String>
+): Localization.() -> String {
+    defaultLocalization.strings[name] = defaultValue
+    for ((locale, value) in localeToValue().entries) {
+        val localization =
+            localizationMap[locale] ?: throw RuntimeException("There is no locale $locale")
+        localization.strings[name] = value
+    }
+    return fun Localization.(): String {
+        return this.strings[name] ?: defaultLocalization.strings[name]
+        ?: throw RuntimeException("There is no string called $name in localization $this")
+    }
+}
+
+fun NonTranslatable(name: String, defaultValue: String): Localization.() -> String {
+    defaultLocalization.strings[name] = defaultValue
+    return fun Localization.(): String {
+        return defaultLocalization.strings[name]
+            ?: throw RuntimeException("There is no string called $name in localization default")
+    }
+}
+
+val LocalLocalization = compositionLocalOf { defaultLocalization }
+
+object Vocabulary {
+    val localization: Localization
+        @Composable
+        @ReadOnlyComposable
+        get() = LocalLocalization.current
+}
+
+@Composable
+fun Localization(locale: Locale, content: @Composable () -> Unit) {
+    CompositionLocalProvider(
+        LocalLocalization provides (localizationMap[locale] ?: defaultLocalization),
+        content = content
+    )
+}
