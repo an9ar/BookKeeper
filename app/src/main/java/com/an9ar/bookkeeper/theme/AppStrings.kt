d57b805a76b6ebf41983e4a7840edd7e24985c42
Dmitriy <63235125+an9ar@users.noreply.github.com>
2021-03-21 22:27:44 +0300

    Added tabs strings to AppStrings file

---
+++ app/src/main/java/com/an9ar/bookkeeper/theme/AppStrings.kt
@@ -0,0 +1,30 @@
+package com.an9ar.bookkeeper.theme
+
+import com.an9ar.bookkeeper.utils.NonTranslatable
+import com.an9ar.bookkeeper.utils.Translatable
+import com.an9ar.bookkeeper.utils.registerSupportedLocales
+import java.util.*
+
+val ENGLISH = Locale("en")
+val RUSSIAN = Locale("ru")
+
+val supportedLocalesNow = registerSupportedLocales(ENGLISH, RUSSIAN)
+
+val booktype_in_progress = Translatable("in_progress", "In progress") {
+    hashMapOf(
+        ENGLISH to "In progress",
+        RUSSIAN to "Читаются"
+    )
+}
+val booktype_reading_list = Translatable("reading_list", "Reading list") {
+    hashMapOf(
+        ENGLISH to "Reading list",
+        RUSSIAN to "В списке для чтения"
+    )
+}
+val booktype_read = Translatable("read", "Read") {
+    hashMapOf(
+        ENGLISH to "Read",
+        RUSSIAN to "Прочитано"
+    )
+}
