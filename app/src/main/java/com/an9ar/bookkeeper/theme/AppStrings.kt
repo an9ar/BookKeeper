60c06f81cec1dd34b68bdbe6f47461497971980e
Dmitriy <63235125+an9ar@users.noreply.github.com>
2021-03-31 21:37:55 +0300

    Added Tabs titles to locales file

--- app/src/main/java/com/an9ar/bookkeeper/theme/AppStrings.kt
+++ app/src/main/java/com/an9ar/bookkeeper/theme/AppStrings.kt
@@ -5,25 +5,37 @@ const val RUSSIAN = "RUSSIAN"
 
 val StringsMap = hashMapOf<String, Map<Strings, String>>(
     ENGLISH to hashMapOf (
-        Strings.BookTypeInProgress to "In progress"
+        Strings.BookTypeRead to "Read",
+        Strings.BookTypeInProgress to "In progress",
+        Strings.BookTypeReadingList to "Reading list"
     ),
     RUSSIAN to hashMapOf (
-        Strings.BookTypeInProgress to "Читаются"
+        Strings.BookTypeRead to "Прочитано",
+        Strings.BookTypeInProgress to "Читаются",
+        Strings.BookTypeReadingList to "В списке для чтения"
     )
 )
 
 sealed class Strings {
+    object BookTypeRead : Strings()
     object BookTypeInProgress : Strings()
+    object BookTypeReadingList : Strings()
 }
 
 interface AppStrings {
+    val bookTypeRead: String
     val bookTypeInProgress: String
+    val bookTypeReadingList: String
 }
 
 fun russianLocale(): AppStrings = object : AppStrings {
+    override val bookTypeRead: String = StringsMap[RUSSIAN]?.get(Strings.BookTypeRead) ?: ""
     override val bookTypeInProgress: String = StringsMap[RUSSIAN]?.get(Strings.BookTypeInProgress) ?: ""
+    override val bookTypeReadingList: String = StringsMap[RUSSIAN]?.get(Strings.BookTypeReadingList) ?: ""
 }
 
 fun englishLocale(): AppStrings = object : AppStrings {
+    override val bookTypeRead: String = StringsMap[ENGLISH]?.get(Strings.BookTypeRead) ?: ""
     override val bookTypeInProgress: String = StringsMap[ENGLISH]?.get(Strings.BookTypeInProgress) ?: ""
+    override val bookTypeReadingList: String = StringsMap[ENGLISH]?.get(Strings.BookTypeReadingList) ?: ""
 }
