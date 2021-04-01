c5e75faeb2b4aab59f063287ee7e29dcc3bbf008
Dmitriy <63235125+an9ar@users.noreply.github.com>
2021-04-01 17:46:06 +0300

    Added locale changing buttons to SettingsScreen

--- app/src/main/java/com/an9ar/bookkeeper/screens/SettingsScreen.kt
+++ app/src/main/java/com/an9ar/bookkeeper/screens/SettingsScreen.kt
@@ -1,9 +1,12 @@
 package com.an9ar.bookkeeper.screens
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
 import androidx.compose.material.Scaffold
@@ -15,6 +18,9 @@ import androidx.compose.ui.Modifier
 import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.navigation.NavHostController
 import com.an9ar.bookkeeper.theme.AppTheme
+import com.an9ar.bookkeeper.theme.LocalStrings
+import com.an9ar.bookkeeper.theme.englishLocale
+import com.an9ar.bookkeeper.theme.russianLocale
 import dev.chrisbanes.accompanist.insets.LocalWindowInsets
 import dev.chrisbanes.accompanist.insets.toPaddingValues
 
@@ -30,7 +36,7 @@ fun SettingsScreen(navHostController: NavHostController) {
             ) {
                 val (screenTitle, backButton) = createRefs()
                 Text(
-                    text = "Settings",
+                    text = AppTheme.strings.settings,
                     style = AppTheme.typography.h6,
                     color = AppTheme.colors.text,
                     modifier = Modifier.constrainAs(screenTitle) {
@@ -64,5 +70,27 @@ fun SettingsScreen(navHostController: NavHostController) {
 
 @Composable
 fun SettingsScreenContent() {
-
+    val localeItemsList = listOf(
+        "English" to englishLocale(),
+        "Russian" to russianLocale(),
+    )
+    LazyColumn(
+        contentPadding = LocalWindowInsets.current.navigationBars
+            .toPaddingValues(
+                bottom = false,
+                additionalTop = AppTheme.sizes.small,
+                additionalBottom = AppTheme.sizes.bottomNavigationHeight
+            ),
+        modifier = Modifier
+            .fillMaxSize()
+    ) {
+        items(localeItemsList) { menuItem ->
+            MenuListItem(
+                itemTitle = menuItem.first,
+                onClickAction = {
+                    LocalStrings.provides(menuItem.second)
+                }
+            )
+        }
+    }
 }
